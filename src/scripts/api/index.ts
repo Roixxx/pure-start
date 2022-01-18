bcf8207b3834acbd1c26e2ac7d1b5edb3cd5d852
Maxim <49519905+Roixxx@users.noreply.github.com>
2022-01-18 12:25:20 +0500

    пробелы в табы

--- src/scripts/api/index.ts
+++ src/scripts/api/index.ts
@@ -2,20 +2,20 @@ import Popup from './popup';
 
 // global API class
 class API {
-  static pageOffset = 0;
+	static pageOffset = 0;
 
-  // disable page scroll
-  static hideOverflow(): void {
-    API.pageOffset = window.scrollY;
-    document.body.style.cssText = `position: fixed; margin-top: ${-API.pageOffset}px`;
-  }
+	// disable page scroll
+	static hideOverflow(): void {
+		API.pageOffset = window.scrollY;
+		document.body.style.cssText = `position: fixed; margin-top: ${-API.pageOffset}px`;
+	}
 
-  // enable page scroll
-  static showOverflow(): void {
-    document.body.style.cssText = 'position: static; margin-top: 0';
-    window.scrollTo(0, API.pageOffset);
-    API.pageOffset = 0;
-  }
+	// enable page scroll
+	static showOverflow(): void {
+		document.body.style.cssText = 'position: static; margin-top: 0';
+		window.scrollTo(0, API.pageOffset);
+		API.pageOffset = 0;
+	}
 }
 
 export default API;
--- src/scripts/api/popup.ts
+++ src/scripts/api/popup.ts
@@ -2,79 +2,79 @@ import API from './index';
 
 // popup class interface
 interface IPopup {
-  $popup: HTMLElement;
-  readonly isShown: boolean;
-  show(): void;
-  hide(): void;
-  addClosePopupEventListeners: () => void;
+	$popup: HTMLElement;
+	readonly isShown: boolean;
+	show(): void;
+	hide(): void;
+	addClosePopupEventListeners: () => void;
 }
 
 // popup contructor options type
 type PopupOptions = {
-  $popup: HTMLElement;
-  onHide?: () => void;
-  onShow?: () => void;
+	$popup: HTMLElement;
+	onHide?: () => void;
+	onShow?: () => void;
 };
 
 // popup handling class
 class Popup implements IPopup {
-  $popup: HTMLElement;
+	$popup: HTMLElement;
 
-  onHide?: () => void;
+	onHide?: () => void;
 
-  onShow?: () => void;
+	onShow?: () => void;
 
-  constructor(options: PopupOptions) {
-    this.$popup = options.$popup;
-    this.onHide = options.onHide;
-    this.onShow = options.onShow;
+	constructor(options: PopupOptions) {
+		this.$popup = options.$popup;
+		this.onHide = options.onHide;
+		this.onShow = options.onShow;
 
-    this.addClosePopupEventListeners();
-  }
+		this.addClosePopupEventListeners();
+	}
 
-  // return true if popup is shown, otherwise false
-  get isShown(): boolean {
-    return this.$popup.classList.contains('popup--opened');
-  }
+	// return true if popup is shown, otherwise false
+	get isShown(): boolean {
+		return this.$popup.classList.contains('popup--opened');
+	}
 
-  // show popup
-  show(): void {
-    const { $popup, onShow } = this;
+	// show popup
+	show(): void {
+		const { $popup, onShow } = this;
 
-    $popup.style.display = 'block';
-    API.hideOverflow();
+		$popup.style.display = 'block';
+		API.hideOverflow();
 
-    setTimeout(() => {
-      $popup.classList.add('popup--opened');
-      onShow?.();
-    });
-  }
+		setTimeout(() => {
+			$popup.classList.add('popup--opened');
+			onShow?.();
+		});
+	}
 
-  // hide popup
-  hide(): void {
-    const { $popup, onHide } = this;
+	// hide popup
+	hide(): void {
+		const { $popup, onHide } = this;
 
-    $popup.classList.remove('popup--opened');
-    setTimeout(() => {
-      $popup.style.display = 'none';
-      API.showOverflow();
-      onHide?.();
-    }, 300);
-  }
+		$popup.classList.remove('popup--opened');
+		setTimeout(() => {
+			$popup.style.display = 'none';
+			API.showOverflow();
+			onHide?.();
+		}, 300);
+	}
 
-  // handle closing popup
-  addClosePopupEventListeners(): void {
-    this.$popup.querySelectorAll('.js-close-popup').forEach((el: Element): void => {
-      el.addEventListener('click', (e: Event): void => {
-        if (
-          !(<HTMLElement>e.target).closest('.popup__window') ||
-          !(<HTMLElement>e.currentTarget).classList.contains('popup__container')
-        ) {
-          this.hide();
-        }
-      });
-    });
-  }
+	// handle closing popup
+	addClosePopupEventListeners(): void {
+		this.$popup.querySelectorAll('.js-close-popup').forEach((el: Element): void => {
+			el.addEventListener('click', (e: Event): void => {
+				if (
+					!(<HTMLElement>e.target).closest('.popup__window') ||
+					!(<HTMLElement>e.currentTarget).classList.contains('popup__container')
+				) {
+					this.hide();
+				}
+			});
+		});
+	}
 }
 
 export default Popup;
